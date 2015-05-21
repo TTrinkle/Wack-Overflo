74bbe44014139ae80120bcb3c8af5fe72de96b0a
Aristides Perez <aris.a.perez@gmail.com>
2015-05-21 19:01:48 -0400

    Changes root route name and some files and folders to fix partial display

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
-  root 'question#index'
+  root 'questions#index'
 
   resource :users
 
