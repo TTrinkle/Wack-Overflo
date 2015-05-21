1c4038345d10994ac72b503bb1b3f80c3b337748
Tanya Trinkle <tanya.trinkle@gmail.com>
2015-05-21 18:03:18 -0400

    Adds routes to answer controller

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,34 @@
+class AnswersController < ApplicationController
+
+	def create
+		@answer = Answer.new(answer_params)
+		if @answer.save
+			redirect_to root_path
+		else
+			redirect_to question_path
+	end
+
+	def new
+		@answer = Answer.new
+	end
+
+	def update 
+
+	end
+
+	def edit
+
+	end
+
+	def destroy
+
+	end
+
+
+	private
+
+	def answer_params
+		params[:answer][:user_id] = session[:id]
+		params[:answer][:question_id] = params[:question_id]
+		params.require(:answer).permit(:body, :user_id, :question_id)
+end
