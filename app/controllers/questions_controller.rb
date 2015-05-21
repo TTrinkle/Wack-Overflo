6548858fe0768c6ca232fdea6d7be9085711970c
Aristides Perez <aris.a.perez@gmail.com>
2015-05-21 19:46:49 -0400

    Completes all basic question func

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :require_login, only: [:new, :create, :edit, :update, :destroy]
+  before_action :get_params, only: [:create, :update]
 
   def index
     @questions = Question.all
@@ -10,15 +11,33 @@ class QuestionsController < ApplicationController
   end
 
   def create
+    new_question = Question.new(get_params)
+    if new_question.save
+      flash[:success] = "Your question was saved"
+      redirect_to root_path
+    else
+      flash[:error] = "Your question could not be saved"
+      redirect_to new_question_path
+    end
   end
 
   def show
+    @question = Question.find(params[:id])
   end
 
   def edit
+    @question = Question.find(params[:id])
   end
 
   def update
+    edited_question = Question.find(params[:id])
+    if edited_question.update_attributes(get_params)
+      flash[:success] = "Your question was updated"
+      redirect_to root_path
+    else
+      flash[:error] = "Your question could not be updated"
+      redirect_to new_question_path
+    end
   end
 
   def destroy
@@ -29,6 +48,10 @@ class QuestionsController < ApplicationController
   def require_login
     return true #unless is_authenticated?(params[:user_id])
     # redirect_to login w/ flash message or something like this
+  end
+
+  def get_params
+    params.require(:question).permit(:title, :body, :score).merge(user_id: 1)
   end
 
 end
