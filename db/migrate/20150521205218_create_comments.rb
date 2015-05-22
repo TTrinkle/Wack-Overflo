e73efd4c40d56dbed5f35853012823dd85c0d5bd
Tanya Trinkle <tanya.trinkle@gmail.com>
2015-05-22 11:42:38 -0400

    Updating voter and author in database

--- db/migrate/20150521205218_create_comments.rb
+++ db/migrate/20150521205218_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-    	t.integer :author, null: false
+    	t.integer :author_id, null: false
     	t.text :body, null: false
     	t.references :commentable, polymorphic: true, index: true
     	
