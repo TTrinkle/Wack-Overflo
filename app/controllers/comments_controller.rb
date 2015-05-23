a34128f02da86aa2cc449878bdf7e6b45315a984
Tanya Trinkle <tanya.trinkle@gmail.com>
2015-05-23 16:39:43 -0400

    Made refactoring changes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,40 +1,38 @@
 class CommentsController < ApplicationController
   before_action :require_login, only: [:new, :create]
 
-  def new
-    # render text: params
-    @comment = Comment.new
-  end
-
   def create
     if params[:question]
-      q = Question.find_by(id: params[:question])
-      new_comment = q.comments.build(body: params[:body], author_id: current_user.id)
+      question = Question.find_by id: params[:question]
+      new_comment = question.comments.build(body: params[:body], author_id: current_user.id)
       new_comment.save
-      redirect_to(:back)
+      redirect_to :back
     elsif params[:answer]
-      a = Answer.find_by(id: params[:answer])
-      new_comment = a.comments.build(body: params[:body], author_id: current_user.id)
+      answer = Answer.find_by id: params[:answer] 
+      new_comment = answer.comments.build(body: params[:body], author_id: current_user.id)
       new_comment.save
-      redirect_to(:back)
+      redirect_to :back
     else
-      c = Comment.find_by(id: params[:comment])
-      new_comment = c.comments.build(body: params[:body], author_id: current_user.id)
+      comment = Comment.find_by id: params[:comment]
+      new_comment = comment.comments.build(body: params[:body], author_id: current_user.id)
       new_comment.save
-      redirect_to(:back)
+      redirect_to :back
     end
   end
 
   def destroy
-    comment = Comment.find_by(id: params[:comment_id])
+    comment = Comment.find_by id: params[:comment_id] 
     if current_user.id == comment.author_id
       comment.delete
-      comment.save
-      flash[:success] = "Your comment was deleted"
-      redirect_to(:back)
+      if comment.save
+        flash[:success] = "Your comment was deleted"
+        redirect_to :back
+      else 
+        flash[:error] = "Your comment could not be deleted"
+      end
     else
       flash[:error] = "You are not authorized to delete this comment."
-      redirect_to(:back)
+      redirect_to :back
     end
   end
 
@@ -43,7 +41,7 @@ class CommentsController < ApplicationController
   def require_login
     return true unless !logged_in?
     else flash[:error] = "You must be logged in to do that."
-    redirect_to(:back)
+    redirect_to :back
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,31 +3,27 @@ class User < ActiveRecord::Base
 
   has_many :questions
   has_many :answers, foreign_key: 'author_id'
-  has_many :comments, foreign_key: 'author'
+  has_many :comments, foreign_key: 'author_id'
   has_many :votes, foreign_key: 'voter'
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
-  # validates :password_digest, presence: true
+  validates :password_digest, presence: true
 
   def my_answers
-    id = User.find_by(id: self.id)
-  	Answer.where(author: id).count
+    User.find_by(id: self.id).answers.count
   end
 
   def my_comments
-    id = User.find_by(id: self.id)
-  	Comment.where(author: id).count
+    User.find_by(id: self.id).comments.count
   end
 
   def my_questions
-    id = User.find_by(id: self.id)
-  	Question.where(user: id).count
+    User.find_by(id: self.id).questions.count
   end
 
   def my_unanswered_questions
-    user = User.find_by(id: self.id)
-    questions = Question.where(user: user)
+    questions = User.find_by(id: self.id).questions
     unanswered = questions.select {|question| question.answers.count == 0 }
     unanswered.count
   end
